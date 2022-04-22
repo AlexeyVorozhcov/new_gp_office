1bff7f6f7a40b1ee52241aa54557867ffa6fc94b
AlexeyVorozhcov <alexey.vorozhcov@gmail.com>
2022-04-22 13:39:18 +0300

    0

--- GP_OFFICE/GP_OFFICE/settings.py
+++ GP_OFFICE/GP_OFFICE/settings.py
@@ -132,9 +132,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django-crispy-forms
+# https://django-crispy-forms.readthedocs.io/en/latest/install.html#installing-django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'uni_form'
+# end django-crispy-forms
